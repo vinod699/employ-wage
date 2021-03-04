1718577761343f6e088fec8076faa768518c7abc
vinod699 <kosarajuvinod9999@gmail.com>
2021-03-04 16:45:56 +0530

    empwage.sh

--- empwage.sh
+++ empwage.sh
@@ -1,27 +1,34 @@
-#! /bin/bash
-IS_PART_TIME=1
-IS_FULL_TIME=2
-totalSalary=0
-EMP_RATE_PER_HR=20
-NUM_WORKING_DAY=20
+#!/bin/bash -x
 
-for (( day=1; day<=NUM_WORKING_DAY; day++ ))
+#constants are written in Captital letters only
+IS_FULLTIME=1
+IS_PARTTIME=2
+EMP_RATE_PER_HR=20;
+NUMBER_OF_WORKING_DAYS=20
+MAX_HRS_IN_MONTH=100
+
+#variable
+totalEmpHrs=0
+totalWorkingDays=0
+
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUMBER_OF_WORKING_DAYS ]]
 do
-  empCheck=$((RANDOM%3))
-    case $empCheck in
-           $IS_FULL_TIME)
-               empHrs=16
-                ;;
-           $IS_PART_TIME)
-               empHrs=8
-               ;;
-           *)
-            empHrs=0
-               ;;
-    esac
+   ((totalWorkingDays++))
+   empCheck=$((RANDOM%3))
+   case $empCheck in
+      $IS_FULLTIME)
+         empHrs=8
+      ;;
+      $IS_PARTTIME)
+         empHrs=4
+      ;;
+      *)
+         empHrs=0
+      ;;
+   esac
+   totalEmpHrs=$(($totalEmpHrs+$empHrs))
 
-    salary=$(( empHrs * EMP_RATE_PER_HR ))
-    totalSalary=$(( totalSalary + salary )) 
-done 
-echo $salary
+done
+
+totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR))
 echo $totalSalary
