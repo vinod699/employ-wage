eddd6ce0fd0cf61949d39450268ddbb7522868c8
vinod699 <kosarajuvinod9999@gmail.com>
2021-03-04 15:47:36 +0530

    usingswitchcase

--- empwage.sh
+++ empwage.sh
@@ -4,14 +4,16 @@ IS_FULL_TIME=2
 EMP_RATE_PER_HR=20
 randomCheck=$((RANDOM%3))
 
-if [ $IS_FULL_TIME -eq $randomCheck ]
-then
-    empHrs=16
-elif [ $IS_PART_TIME -eq $randomCheck ]
-then
-    empHrs=8
-else
-   empHrs=0
-fi
+case $randomCheck in
+         $IS_FULL_TIME)
+                empHrs=16
+                 ;;
+         $IS_PART_TIME) 
+               empHrs=8
+                ;;
+         *)
+         empHrs=0
+          ;;
+esac
 salary=$(($empHrs*$EMP_RATE_PER_HR))
 echo $salary
