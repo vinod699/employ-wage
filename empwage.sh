56e002b836e35b2dffa8b27577f653aeb21f519d
vinod699 <kosarajuvinod9999@gmail.com>
2021-03-04 16:39:13 +0530

    empwage.sh

--- empwage.sh
+++ empwage.sh
@@ -1,19 +1,27 @@
-#!/bin/bash
+#! /bin/bash
 IS_PART_TIME=1
 IS_FULL_TIME=2
+totalSalary=0
 EMP_RATE_PER_HR=20
-randomCheck=$((RANDOM%3))
+NUM_WORKING_DAY=20
 
-case $randomCheck in
-         $IS_FULL_TIME)
-                empHrs=16
-                 ;;
-         $IS_PART_TIME) 
-               empHrs=8
+for (( day=1; day<=NUM_WORKING_DAY; day++ ))
+do
+  empCheck=$((RANDOM%3))
+    case $empCheck in
+           $IS_FULL_TIME)
+               empHrs=16
                 ;;
-         *)
-         empHrs=0
-          ;;
-esac
-salary=$(($empHrs*$EMP_RATE_PER_HR))
+           $IS_PART_TIME)
+               empHrs=8
+               ;;
+           *)
+            empHrs=0
+               ;;
+    esac
+
+    salary=$(( empHrs * EMP_RATE_PER_HR ))
+    totalSalary=$(( totalSalary + salary )) 
+done 
 echo $salary
+echo $totalSalary
